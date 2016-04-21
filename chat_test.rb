1f5440d0ed8bddfec13d03964c3af9f355566450
Kalior <kalioragus@gmail.com>
2016-04-21 14:53:45 +0200

    Add script that creates game and changes name

--- chat_test.rb
+++ chat_test.rb
@@ -4,17 +4,11 @@
 require 'watir-webdriver'
 
 module ChatTest
-
-  def chat_test (browser)
-    prng = Random.new
-    browser.alert.set "Watir" + prng.rand(10000).to_s
-    browser.alert.ok
-
+  def chat_test browser
     browser.text_field(id: 'input-field-main').wait_until_present
     for i in 1..25
       browser.text_field(id: 'input-field-main').set "Chatting whoho"
       browser.text_field(id: 'input-field-main').send_keys :enter
     end
-    browser.close
   end
 end
--- tests.rb
+++ tests.rb
@@ -1,19 +1,35 @@
 #! /usr/bin/ruby
 require 'watir-webdriver'
 
+# Our test scripts
 require './chat_test.rb'
+require './game_test.rb'
 
 class Tests
   include ChatTest
+  include GameTest
   def chat
     threads = []
-    for i in 0..5
-      threads << Thread.new {
+    for i in 0...5
+      threads << Thread.new do
         browser = Watir::Browser.new :chrome
         browser.goto 'http://hastings.se'
         Random.new_seed
-        chat_test browser
-      }
+        prng = Random.new
+        browser.alert.set "Watir" + prng.rand(10000).to_s
+        browser.alert.ok
+        browser.text_field(id: 'input-field-main').wait_until_present
+
+        case prng.rand(2)
+        when 0
+          chat_test browser
+        when 1
+          create_game browser
+        else
+          chat_test browser
+        end
+        browser.close
+      end
     end
     threads.each { |thr| thr.join }
   end
